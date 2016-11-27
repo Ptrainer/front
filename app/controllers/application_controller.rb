6c1e8eac5e7a036c412946594a9af8552f739a12
Tristan Sandras <tristan@sandras.fr>
2016-11-27 12:08:10 +0100

    :door: Devise configuration (1/3) and add theme for sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@
 # To abstract ActiveController in Website
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  helper_method :resource_name, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,27 +2,42 @@
 #
 # Table name: users
 #
-#  id                   :integer          not null, primary key
-#  first_name           :string
-#  last_name            :string
-#  email                :string           not null
-#  password             :string           not null
-#  identification_token :string
-#  validation_date      :datetime
-#  language_id          :integer
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
+#  id                     :integer          not null, primary key
+#  first_name             :string
+#  last_name              :string
+#  email                  :string           default(""), not null
+#  validation_date        :datetime
+#  language_id            :integer
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  failed_attempts        :integer          default(0), not null
+#  unlock_token           :string
+#  locked_at              :datetime
 #
-# Index: email, identification_token
 
 # app/models/user.rb
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable
   has_one :client
   has_one :coach
 
-  validates_presence_of :email, :password
-  validates_uniqueness_of :identification_token, allow_nil: true
-  validates_uniqueness_of :email
+  # validates_presence_of :email
+  # validates_uniqueness_of :email
   validate :unique_users_type
 
   def unique_users_type
